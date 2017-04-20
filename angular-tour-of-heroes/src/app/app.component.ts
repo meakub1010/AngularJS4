84228cdf932c0672b691cf04bf0d7f03524a616f
Muhammad Eakub <meakub86@gmail.com>
2017-04-20 17:10:14 -0400

    initial set up for angular tour of heroes app

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -9,13 +9,66 @@ export class Hero {
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span>{{hero.name}}
+      </li>
+    </ul>
     <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    `
+    <div><label>ID: </label>{{hero.id}}</div>
+    <div><label>Name: </label>
+    <input [(ngModel)] = "hero.name" placeholder="Name"></div>
+    `,
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -23,4 +76,13 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
 }
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Ehan'},
+  {id: 12, name: 'Chintu'},
+  {id: 13, name: 'Shinu'},
+  {id: 14, name:'Minu'},
+  {id: 15, name:'Chinta'}];
+
