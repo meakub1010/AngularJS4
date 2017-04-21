f39edb72837e42fa942540c097dfcc36539d3f61
Muhammad Eakub <meakub86@gmail.com>
2017-04-21 18:23:26 -0400

    modified app component, module and added getHero method to HeroService

--- angular-tour-of-heroes/src/app/app.module.js
+++ angular-tour-of-heroes/src/app/app.module.js
@@ -7,10 +7,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms"); // <-- NgModel lives here
 var app_component_1 = require("./app.component");
+var heroes_component_1 = require("./heroes.component");
 var hero_detail_component_1 = require("./hero-detail.component");
+var dashboard_component_1 = require("./dashboard.component");
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -20,12 +24,34 @@ AppModule = __decorate([
     core_1.NgModule({
         imports: [
             platform_browser_1.BrowserModule,
-            forms_1.FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+            forms_1.FormsModule,
+            router_1.RouterModule.forRoot([
+                {
+                    path: 'heroes',
+                    component: heroes_component_1.HeroesComponent
+                },
+                {
+                    path: 'dashboard',
+                    component: dashboard_component_1.DashboardComponent
+                },
+                {
+                    path: 'detail/:id',
+                    component: hero_detail_component_1.HeroDetailComponent
+                },
+                {
+                    path: '',
+                    redirectTo: '/dashboard',
+                    pathMatch: 'full'
+                },
+            ])
         ],
         declarations: [
             app_component_1.AppComponent,
+            dashboard_component_1.DashboardComponent,
+            heroes_component_1.HeroesComponent,
             hero_detail_component_1.HeroDetailComponent
         ],
+        providers: [hero_service_1.HeroService],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,19 +1,51 @@
 import { NgModule }      from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component'
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot([
+                {
+                  path: 'heroes',
+                  component: HeroesComponent
+                },
+                {
+                  path: 'dashboard',
+                  component: DashboardComponent
+                },
+                {
+                  path: 'detail/:id',
+                  component: HeroDetailComponent
+                },
+                {
+                  path: '',
+                  redirectTo: '/dashboard',
+                  pathMatch: 'full'
+                },
+            ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
+  providers:[ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+
+
+export class AppModule { 
+
+}
