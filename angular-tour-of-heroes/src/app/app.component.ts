36c5c66cbb3fc24f540811002a529bde9f5f0212
Muhammad Eakub <meakub86@gmail.com>
2017-05-04 16:43:49 -0400

    added items link on the nav

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
         <nav>
             <a routerLink="/dashboard" routerLinkActivate="active">Dashboard</a>
             <a routerLink="/heroes" routerLinkActive="active" >Heroes</a>
+            <a routerLink="/items" routerLinkActive="active" >Items</a>
         </nav>
         <router-outlet></router-outlet>
     `
