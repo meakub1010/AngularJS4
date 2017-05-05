2436640e44647146a491c864206c0dc62d9ee30a
Muhammad Eakub <meakub86@gmail.com>
2017-05-05 12:00:35 -0400

    updated highlight directive

--- angular-tour-of-heroes/src/app/highlight.directive.ts
+++ angular-tour-of-heroes/src/app/highlight.directive.ts
@@ -1,2 +1,10 @@
 // this is an attribute directive
+// will be added as attribute to html element and should change the appearance
 
+import { Directive, ElementRef, Input } from '@angular/core';
+
+class HighlightDirective {
+    constructor(el: ElementRef){
+        el.nativeElement.style.color = 'red';
+    }
+}
